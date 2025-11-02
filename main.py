6633bbdb1944d3f47365444807a48eb43174aa2f
wasabina67 <wasabina67@gmail.com>
2025-11-02 19:04:49 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,35 @@
+import gradio as gr
+from ollama import Client
+
+client = Client(host='http://localhost:11434')
+
+
+def translate(text):
+    messages = [
+        {},
+        {},
+    ]
+    stream = client.chat(
+        model="",
+        messages=messages,
+        stream=True,
+    )
+
+    result = ""
+    for chunk in stream:
+        result += chunk.message.content
+        yield result
+
+
 def main():
-    print("Hello from gradio-example!")
+    app = gr.Interface(
+        fn=translate,
+        inputs=gr.Textbox(lines=10, max_lines=40, label="English"),
+        outputs=gr.Textbox(lines=10, max_lines=40, show_copy_button=True, label="Japanese"),
+        title="English to Japanese translation app",
+        description="",
+    )
+    app.launch()
 
 
 if __name__ == "__main__":
